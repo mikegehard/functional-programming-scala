3ec336acb323287228918486f9000eaeacabb7a7
Mike Gehard <mike.gehard@livingsocial.com>
2012-06-14 16:07:52 -0600

    Finish ex 2.5.2

--- ex2-5.scala
+++ ex2-5.scala
@@ -1,1 +1,12 @@
+// Ex 2.5.1
 val abs: (Int => Int) = num => if(num < 0) -num else num
+
+// Ex 2.5.2
+def isEven(n: Int): Boolean = 
+  n % 2 == 0
+
+def not(fun: Int => Boolean): Int => Boolean = 
+  a => !fun(a)
+
+val isOdd: Int => Boolean = not(isEven)
+
