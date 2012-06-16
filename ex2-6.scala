6980f46a09dbfd934c63d3fbf17d0cc0ee000b15
Mike Gehard <mike.gehard@gmail.com>
2012-06-16 08:43:00 -0600

    Implement lift and use it

--- ex2-6.scala
+++ ex2-6.scala
@@ -14,5 +14,11 @@ def isDivisibleBy3And5(num: Int): Boolean =
 def isDivisibleBy3Or5(num: Int): Boolean =
   isDivisibleBy(3).apply(num) || isDivisibleBy(5).apply(num) 
 
-// def lift(): =
+def lift[T](a: Pred[T], b: Pred[T], combine: (Boolean, Boolean) => Boolean): Pred[T] =
+  (x: T) => combine(a(x), b(x))
   
+def isDivisibleBy3And5UseLift(num: Int): Boolean = 
+  lift(isDivisibleBy(3), isDivisibleBy(5), _ && _).apply(num)
+
+def isDivisibleBy3Or5UseLift(num: Int): Boolean = 
+  lift(isDivisibleBy(3), isDivisibleBy(5), _ || _).apply(num)
